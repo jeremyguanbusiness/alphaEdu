dd4076836ee56ac68a2afa657cc76a3ca885b87a
Jeremy <jeremyguanbusiness@gmail.com>
2018-10-05 23:11:07 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Blog />
         </div>
